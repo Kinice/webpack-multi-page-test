c8b31061372a5b7a0054020d2eae9698e0ae3b14
Kinice <szp93@126.com>
2017-05-17 18:12:47 +0800

    remove all

--- dist/js/vendors.js
+++ dist/js/vendors.js
@@ -86,7 +86,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "a7f7309ec54f6b1e967a"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "050e1440ebb1107183aa"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotMainModule = true; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
